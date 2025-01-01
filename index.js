bd6e1e491e5c127f6bf99222588ec0bd8f9d6409
Feyad Elahmed <feyadelahmed@gmail.com>
2025-01-01 22:05:10 +0300

    port

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use((error, req, res, next) => {
   })
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
